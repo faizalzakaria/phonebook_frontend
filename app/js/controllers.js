dd5a56a489825274cb79d918d599be9f87596074
Faizal Zakaria <phaibusiness@gmail.com>
2014-06-06 15:41:41 +0800

    Fixed creation

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -6,18 +6,15 @@ angular.module('myApp.controllers', [])
   .controller('PhoneBookListCtrl', ['$scope', 'PhoneBooksFactory', 'PhoneBookFactory', '$location',
     function ($scope, PhoneBooksFactory, PhoneBookFactory, $location) {
 
-      /* callback for ng-click 'editPhoneBook': */
       $scope.editPhoneBook = function (phoneBookId) {
         $location.path('/phoneBook-detail/' + phoneBookId);
       };
 
-      /* callback for ng-click 'deletePhoneBook': */
       $scope.deletePhoneBook = function (phoneBookId) {
         PhoneBookFactory.delete({ id: phoneBookId });
         $scope.phoneBooks = PhoneBooksFactory.query();
       };
 
-      /* callback for ng-click 'createPhoneBook': */
       $scope.createNewPhoneBook = function () {
         $location.path('/phoneBook-creation');
       };
@@ -25,7 +22,26 @@ angular.module('myApp.controllers', [])
       $scope.phoneBooks = PhoneBooksFactory.query();
 
     }])
-  .controller('PhoneBookDetailCtrl', ['$scope', function($scope) {
+  .controller('PhoneBookDetailCtrl', ['$scope', '$routeParams', 'PhoneBookFactory', '$location',
+    function ($scope, $routeParams, PhoneBookFactory, $location) {
+
+    $scope.updatePhoneBook = function () {
+      PhoneBookFactory.update($scope.phoneBook);
+      $location.path('/phoneBook-list');
+    };
+
+    $scope.cancel = function () {
+      $location.path('/phoneBook-list');
+    };
+
+    $scope.phoneBook = PhoneBookFactory.show({id: $routeParams.id});
+
   }])
-  .controller('PhoneBookCreationCtrl', ['$scope', function($scope) {
+  .controller('PhoneBookCreationCtrl', ['$scope', '$routeParams', 'PhoneBooksFactory', '$location',
+    function ($scope, $routeParams, PhoneBooksFactory, $location) {
+
+    $scope.createNewPhoneBook = function () {
+      PhoneBooksFactory.create($scope.phoneBook);
+      $location.path('/phoneBook-list');
+    }
   }]);
